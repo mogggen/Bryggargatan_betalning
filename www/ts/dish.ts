294d1c55e212202fabb5a0986723297834372855
Harry Hedman <harhed-9@student.ltu.se>
2020-11-06 14:54:19 +0100

    Removed unnecessary lines in tsconfig.json

---
+++ www/ts/dish.ts
@@ -0,0 +1,10 @@
+class Dish{
+    name: string;
+    price: number;
+    type: string;
+    description: string;
+    is_milk_free: string;
+    is_gluten_free: string;
+    is_egg_free: string;
+    contains_soy: boolean;
+}
