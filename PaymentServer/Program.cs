cd806fd6fb1d0f698a228e40b416f914a9865767
Morgan <melon21@hotmail.se>
2020-11-09 15:41:11 +0100

    Hello World C#!

---
+++ PaymentServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PaymentServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //list<> med sockets
+
+            //
+        }
+    }
+}
