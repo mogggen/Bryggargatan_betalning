f5cd79b2f8ce93f620e0bed15ca3ef2208ea8b2d
Magnus Lind <maglii-9-local@B4711.ltuad.ltu.se>
2020-11-11 12:50:54 +0100

    created send_to_server

---
+++ www/ts/send_to_server.ts
@@ -0,0 +1,10 @@
+function SendToServer(msg: string)
+{
+    const http = new XMLHttpRequest();
+    const url = "http://localhost:9002"
+    http.open("POST", url);
+    http.send(msg);
+    http.onreadystatechange = () =>  {
+        console.log("response: " + http.response);
+    };
+}
